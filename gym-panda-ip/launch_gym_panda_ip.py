931cf5b2fde7901c6e3c579aec5437b9982cc820
Laurent LEQUIEVRE <laurent.lequievre@uca.fr>
2020-11-18 15:15:51 +0100

    Added a main

--- gym-panda-ip/launch_gym_panda_ip.py
+++ gym-panda-ip/launch_gym_panda_ip.py
@@ -11,28 +11,33 @@ client_id = p.connect(p.GUI)
 
 from gym_panda_ip.envs.panda_ip_env import PandaIPEnv
 
-env = PandaIPEnv(client_id)
-env.reset()
+def main():
 
-for _ in range(100):
-    env.render()
-    obs, reward, done, info = env.step(None) # No action, just to test it like that !
-    #obs, reward, done, info = env.step(
-    #   env.action_space.sample()) # take a random action
+    env = PandaIPEnv(client_id)
+    env.reset()
 
-env.close()
-
-print("Enter to quit !")
-while True:
+    for _ in range(100):
+        env.render()
+        obs, reward, done, info = env.step(None) # No action, just to test it like that !
+        #obs, reward, done, info = env.step(
+        #   env.action_space.sample()) # take a random action
     
-    # look if there is keyboard event and put it in a dictionary named keys
-    keys = p.getKeyboardEvents()
-    
-    # 'Enter' event = 65309, if so .. break the loop
-    if 65309 in keys:
-      break
+    env.close()
 
-p.disconnect()
+if __name__ == '__main__':
+    main()
+    
+    print("Enter to quit !")
+    while True:
+    
+        # look if there is keyboard event and put it in a dictionary named keys
+        keys = p.getKeyboardEvents()
+    
+        # 'Enter' event = 65309, if so .. break the loop
+        if 65309 in keys:
+            break
+
+    p.disconnect()
 
 
 
