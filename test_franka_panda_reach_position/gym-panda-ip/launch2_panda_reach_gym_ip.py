5f94a30618e6306d0e1f8fba55d0def676741f8c
Laurent LEQUIEVRE <laurent.lequievre@uca.fr>
2021-03-04 17:40:17 +0100

    Added print infos to debug

--- test_franka_panda_reach_position/gym-panda-ip/launch2_panda_reach_gym_ip.py
+++ test_franka_panda_reach_position/gym-panda-ip/launch2_panda_reach_gym_ip.py
@@ -10,7 +10,6 @@ laurent.lequievre@uca.fr
 
 import gym
 import gym_panda_ip
-import pybullet as p
 
 def main():
     env = gym.make('panda-ip-reach-v0')
@@ -20,10 +19,11 @@ def main():
         observation = env.reset()
         for t in range(100):
             env.render()
-            print(observation)
             action = env.action_space.sample()
             print("action = {0}".format(action))
             observation, reward, done, info = env.step(action)
+            print("observation = {0}".format(observation))
+            print("reward = {0}".format(reward))
             if done:
                 print("Episode finished after {} timesteps".format(t+1))
                 break
